71c9b1233edd45ded2d8dccf0e6a4ad4d720ff41
IndRyan <ryanparkerdomains@gmail.com>
2026-02-05 22:09:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-document.querySelector("form").addEventListener("submit",function(e){e.preventDefault();alert("Thank you! We will contact you soon 😊");});
+const container = document.getElementById("cardscontainer");
+const template = document.getElementById("template-card");
+
+const socialCards = [
+    {
+        title: "Instagram",
+        image: "./images/instagram.jpg",
+        link: "https://www.instagram.com/yourusername"
+    },
+    {
+        title: "Facebook",
+        image: "./images/facebook.jpg",
+        link: "https://www.facebook.com/yourpage"
+    }
+];
+
+socialCards.forEach(item => {
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector("h3").innerText = item.title;
+    clone.querySelector("img").src = item.image;
+    clone.querySelector(".card-link").href = item.link;
+
+    container.appendChild(clone);
+});
